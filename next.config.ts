53625983bf6319a214d689bc167be6c6c9b188a6
Lucas Costa <lucascostacruzsilva@gmail.com>
2026-04-23 09:33:01 -0300

    ajuste de segurança

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,47 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com;
+      connect-src 'self' https://www.google-analytics.com;
+      img-src 'self' data: https://www.google-analytics.com;
+      style-src 'self' 'unsafe-inline';
+      object-src 'none';
+      base-uri 'self';
+      frame-ancestors 'none';
+    `.replace(/\n/g, ""),
+  },
+];
+
 const nextConfig: NextConfig = {
   output: 'standalone',
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
       oneOf: [
-        { resourceQuery: /component/, use: ["@svgr/webpack"] }, // <— componente
-        { type: "asset" },                                      // <— URL p/ <Image/>
+        { resourceQuery: /component/, use: ["@svgr/webpack"] },
+        { type: "asset" },
       ],
     });
     return config;
